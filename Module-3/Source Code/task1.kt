d1f2f1c1eecf27444ca9a212545deb4eb384018f
sbr-hkyr <sbr.hkyr@gmail.com>
2026-09-14 03:18:03 +0800

    Add traffic light and prime number logic in task1.kt

---
+++ Module-3/Source Code/task1.kt
@@ -0,0 +1,67 @@
+fun main() {
+    println(1 == 1)
+    println(1 < 1)
+    
+    var trafficLightColor = "Yellow"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else {
+        println("Go")
+    }
+    
+    trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+    
+    var x = 3
+
+    when (x) {
+        2 -> println("x is a prime number between 1 and 10.")
+        3 -> println("x is a prime number between 1 and 10.")
+        5 -> println("x is a prime number between 1 and 10.")
+        7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+    
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+    
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+    
+    trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+    
+    trafficLightColor = "Black"
+
+    val message = 
+      if (trafficLightColor == "Red") "Stop"
+      else if (trafficLightColor == "Yellow") "Slow"
+      else if (trafficLightColor == "Green") "Go"
+      else "Invalid traffic-light color"
+
+    println(message)
+    
+}
