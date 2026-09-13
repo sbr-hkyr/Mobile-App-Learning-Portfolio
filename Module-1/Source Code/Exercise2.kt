fbb6a90b7690d38a1d5afc1dc32c399bd2f69767
sbr-hkyr <sbr.hkyr@gmail.com>
2026-09-13 17:05:35 +0800

    Add Exercise2.kt with various print statements

---
+++ Module-1/Source Code/Exercise2.kt
@@ -0,0 +1,69 @@
+fun main() {
+    val count: Int = 2
+    println(count)
+    println("You have $count unread messages.")
+    
+    val countt: Int = 10
+    println("You have $countt unread messages.")
+    
+    val unreadCount = 3
+    val readCount = 1900
+    println("You have ${unreadCount + readCount} total messages in your inbox.")
+    
+    val photos = 1453
+    val delPhotos = 382
+    println("$photos photos")
+    println("$delPhotos photos deleted")
+    println("${photos - delPhotos} photos left.")
+    
+    var cartTotal = 0
+    println("Total costs of the cart: $cartTotal")
+    
+    cartTotal = 20
+    println("Total costs of the cart: $cartTotal.")
+    
+    var counting = 2
+    println("$counting")
+    counting = counting + 1
+    println("$counting")
+    
+    var mcount = 1900
+    println("You have $mcount messages.")
+    mcount = mcount + 3
+    println("You have $mcount messages.")
+    
+    var mmcount = 6
+    println("You have $mmcount messages.")
+    mmcount++
+    println("You have $mmcount messages.")
+    
+    var ncount = 8
+    println("You have $ncount messages.")
+    ncount--
+    println("You have $ncount messages.")
+    
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength = trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
+    
+    val nextMeeting = "Next meeting: "
+    val date = "January 1 "
+    val reminder = nextMeeting + date + "at work"
+    println(reminder)
+    
+    println("Say \"hello\"")
+    
+    val notificationsEnabled: Boolean = true
+    println(notificationsEnabled)
+    
+    val notificationsNotEnabled: Boolean = false
+    println("Are notifications enabled? " + notificationsNotEnabled)
+
+    //Ya egzersizleri tek tek yapmayi dusunen aklimi seveyim, yoruldummm.
+}
+    /*
+     * Eveet, hemen odevi bitirip kitap okumak istiyorum,
+     * Daha bitirmem gereken tasarimlar var, yorucusun akademi :(
+     */
