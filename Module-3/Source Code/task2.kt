a9fc19ec6103d0c4de5cdf255b934d7697359a31
sbr-hkyr <sbr.hkyr@gmail.com>
2026-09-14 03:34:58 +0800

    Add main function to display favorite actor's name length
    Check for null and print the length of the favorite actor's name.

---
+++ Module-3/Source Code/task2.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var favoriteActor: String? = "Sandra Oh"
+
+    if (favoriteActor != null) {
+      println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+    } else {
+      println("You didn't input a name.")
+    }
+    
+    val lengthOfName = if (favoriteActor != null) {
+      favoriteActor.length
+    } else {
+      0
+    }
+
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
+}
